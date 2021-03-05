57700fa50cc5e9b69433744b9d7058bfded8ffa0
jayce.feng <fengwenxuan1@baonengmotor.com>
2021-03-06 01:20:27 +0800

    m

---
+++ myapplication/src/main/java/com/example/myapplication/TestUtils.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import android.util.Log;
+
+/**
+ * Copyright (C), 2017-2021, 宝能有限公司
+ * Author: jayce.feng
+ * Date: 2021/3/6
+ * Description:
+ * version v1.0
+ */
+public class TestUtils {
+    public static  void test(){
+        Log.e("-----","test");
+    }
+}
