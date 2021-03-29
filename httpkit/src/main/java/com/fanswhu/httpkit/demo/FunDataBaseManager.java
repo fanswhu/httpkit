2f05f08006e4f4bbed6cbdce175308f674cd57de
jayce.feng <fengwenxuan1@baonengmotor.com>
2021-03-29 14:22:19 +0800

    [fix]发现一个好玩的注释

--- httpkit/src/main/java/com/fanswhu/httpkit/demo/FunDataBaseManager.java
+++ httpkit/src/main/java/com/fanswhu/httpkit/demo/FunDataBaseManager.java
@@ -4,7 +4,28 @@ import com.fanswhu.httpkit.core.BaseDataManager;
 import com.fanswhu.httpkit.core.BaseObserver;
 import com.fanswhu.httpkit.core.HttpCallBack;
 
-
+////////////////////////////////////////////////////////////////////
+//                          _ooOoo_                               //
+//                         o8888888o                              //
+//                         88" . "88                              //
+//                         (| ^_^ |)                              //
+//                         O\  =  /O                              //
+//                      ____/`---'\____                           //
+//                    .'  \\|     |//  `.                         //
+//                   /  \\|||  :  |||//  \                        //
+//                  /  _||||| -:- |||||-  \                       //
+//                  |   | \\\  -  /// |   |                       //
+//                  | \_|  ''\---/''  |   |                       //
+//                  \  .-\__  `-`  ___/-. /                       //
+//                ___`. .'  /--.--\  `. . ___                     //
+//              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+//            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+//            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+//      ========`-.____`-.___\_____/___.-`____.-'========         //
+//                           `=---='                              //
+//      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+//         佛祖保佑       永无BUG     永不修改                  //
+////////////////////////////////////////////////////////////////////
 public class FunDataBaseManager extends BaseDataManager<PostRequest, PostInfo> {
     public FunDataBaseManager(HttpCallBack<PostInfo> callBack) {
         super("getJoke", callBack);
@@ -20,7 +41,7 @@ public class FunDataBaseManager extends BaseDataManager<PostRequest, PostInfo> {
     }
 
 
-    public void getData(String page,String count,String type) {
-        setRequestParams(page,count,type);
+    public void getData(String page, String count, String type) {
+        setRequestParams(page, count, type);
     }
 }
